dc3517de3ed6456b67710e672ea1062b6a4a8c6d
myles <mrmylesmc@gmail.com>
2023-07-09 20:26:37 -0500

    ipy to .py port completed.

--- R/py/main_clone.py
+++ R/py/main_clone.py
@@ -1,105 +1,68 @@
-import urllib.request
+import os
+import requests
 import zipfile
 import geopandas as gpd
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.colors import ListedColormap
+from matplotlib import colormaps, pyplot as plt
 
-# 1. GET COUNTRY BORDERS
-# -----------------------
-print("GET COUNTRY BORDERS")
-resolution = "3"
+# Choices of Country Border geojson Resolutions.
+resolution_choices = ["01M", "03M", "10M", "30M", "60M"]
+
+# resolution_choices[4] is 60M
+res = resolution_choices[4]
+
+# Choose country from ISO code
 country = "US"
+continent = "na"  # na = North America, sa = South America
 
+world_country_borders = gpd.read_file(
+    f"https://gisco-services.ec.europa.eu/distribution/v2/countries/geojson/CNTR_RG_{res}_2020_4326.geojson")
+country_border = world_country_borders[world_country_borders["CNTR_ID"] == country]
 
-def get_country_borders(resolution, country):
-    country_borders = gpd.read_file(
-        "https://gisco-services.ec.europa.eu/distribution/v2/countries/geojson/CNTR_RG_60M_2020_4326.geojson")
+# Clip US border to just CONUS - excludes Alaska, etc.
+if country == "US":
+    country_border = country_border.clip_by_rect(-140, 20, -50, 50)
+    # clip_by_rect returns a geoseries, so turning back to a GDF
+    country_border = gpd.GeoDataFrame(geometry=gpd.GeoSeries(country_border))
+    country_border.plot()
 
-    return country_borders
+# Get Continent-wide Watershed Basin Boundaries
+url = f"https://data.hydrosheds.org/file/HydroBASINS/standard/hybas_{continent}_lev03_v1c.zip"
+file_name = url.split("/")[-1]
 
+r = requests.get(url)
+with open(file_name, 'wb') as outfile:
+    outfile.write(r.content)
 
-country_borders = get_country_borders(resolution, country)
+with zipfile.ZipFile(file_name, 'r') as zip_ref:
+    zip_ref.extractall()
 
-# 2. GET BASINS
-# --------------
-print("GET BASINS")
-
-
-def get_basins():
-    url = "https://data.hydrosheds.org/file/HydroBASINS/standard/hybas_na_lev03_v1c.zip"
-    file_name = "hybas_na_lev03_v1c.zip"
-
-    urllib.request.urlretrieve(url, file_name)
-    with zipfile.ZipFile(file_name, 'r') as zip_ref:
-        zip_ref.extractall()
-
-
-get_basins()
-print("basin downloaded")
-
-file_names = [filename for filename in os.listdir()
-              if filename.endswith(".shp")]
-print("load basin")
-
-
-def load_basins():
-    print("loading filenames")
-    filenames = [
-        filename for filename in file_names if filename.endswith(".shp")]
-    print(filenames)
-    namerica_basin = gpd.read_file(filenames[0])
-
-    return namerica_basin
-
-
-namerica_basin = load_basins()
-print("basin loaded")
-
+# Intersect Basin with only the wanted Country Boundary.
+continent_basin = gpd.read_file(file_name.split(".")[0]+".shp")
 print("Intersect Basin with only the wanted Country Boundary.")
-gpd.sjoin(country_borders, namerica_basin)
+country_basin = gpd.overlay(
+    country_border, continent_basin, how='intersection')
 
-# 3. GET RIVERS DATA
-# -------------------
-print("Get Rivers")
+# Get Rivers
+url = f"https://data.hydrosheds.org/file/HydroRIVERS/HydroRIVERS_v10_{continent}_shp.zip"
+file_name = url.split("/")[-1]
 
+r = requests.get(url)
+with open(file_name, 'wb') as outfile:
+    outfile.write(r.content)
 
-def get_rivers():
-    url = "https://data.hydrosheds.org/file/HydroRIVERS/HydroRIVERS_v10_na_shp.zip"
-    file_name = "na-rivers.zip"
+with zipfile.ZipFile(file_name, 'r') as zip_ref:
+    zip_ref.extractall()
 
-    urllib.request.urlretrieve(url, file_name)
-    with zipfile.ZipFile(file_name, 'r') as zip_ref:
-        zip_ref.extractall()
+continent_rivers = gpd.read_file(os.path.join(
+    f"HydroRIVERS_v10_{continent}_shp", f"HydroRIVERS_v10_{continent}.shp"))
 
-
-get_rivers()
-print("Getting Rivers")
-list_files = [filename for filename in os.listdir(
-    "HydroRIVERS_v10_na_shp") if filename.endswith(".shp")]
-print(list_files)
-
-
-def load_rivers():
-    filenames = [os.path.join("HydroRIVERS_v10_na_shp", filename)
-                 for filename in list_files]
-    print(filenames)
-    namerica_rivers = gpd.read_file(filenames[0])
-
-    return namerica_rivers
-
-
-namerica_rivers = load_rivers()
-
-# 4. DETERMINE BASIN FOR EVERY RIVER
-# ---------------------------------
-
+# Clip rivers to country_basin extent.
 country_river_basin = gpd.overlay(
-    namerica_rivers, namerica_basin, how='intersection')
+    continent_rivers, country_basin, how='intersection')
 
-# 5. RIVER WIDTH
-# --------------
+# RIVER WIDTH config
 
 country_river_basin['ORD_FLOW'].unique()
 
@@ -130,19 +93,20 @@ def assign_river_width(row):
 country_river_basin['width'] = country_river_basin.apply(
     assign_river_width, axis=1)
 
+country_river_basin['HYBAS_ID'].unique()
+
 # 6. PLOT
 # --------
 
-country_river_basin['HYBAS_ID'].unique()
-
-cmap = ListedColormap(cm.get_cmap('Dark2').colors[:14])
-
 fig, ax = plt.subplots(figsize=(7, 7.75))
-country_river_basin.plot(ax=ax, column='HYBAS_ID', cmap=cmap,
-                         linewidth=country_river_basin['width']*10, edgecolor='black', alpha=country_river_basin['width'])
+country_river_basin.plot(ax=ax, column='HYBAS_ID', cmap=colormaps['twilight'],
+                         linewidth=country_river_basin['width'],
+                         #  edgecolor='black',
+                         #  alpha=country_river_basin['width'],
+                         categorical=True,
+                         #  legend=True
+                         )
 ax.set_axis_off()
-plt.title('')
-plt.savefig('us-river-basins.png', dpi=600,
-            bbox_inches='tight', pad_inches=0, transparent=True)
 
-country_river_basin.to_file("us-river-basins.geojson", driver="GeoJSON")
+fig.savefig(f'{country}-river-basins.png', dpi=600,
+            bbox_inches='tight', pad_inches=0, transparent=True)
